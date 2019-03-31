76eed2902c4bde2fe3553ba70b5cbce7171e9dbb
baoletrg <baole2298@gmail.com>
2019-03-31 11:01:53 +0700

    add back run method

--- Horse.js
+++ Horse.js
@@ -4,6 +4,8 @@ function Horse() {
 Horse.prototype.jump = function() {
   console.log('Jumped...');
 };
-
+Horse.prototype.run = function() {
+	console.log('Running....');
+};
 
 module.exports = Horse;
